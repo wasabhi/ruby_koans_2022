305c7c7c25a280c399fb9398fa4b5666c0a664e7
Abhinav Keswani <abhinav.keswani@gmail.com>
2023-02-12 20:15:16 +1300

    Triangle project 1. Complete.

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,14 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  case [a, b, c].uniq.count
+	when 1
+		return :equilateral
+	when 2
+		return :isosceles
+	when 3
+		return :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
